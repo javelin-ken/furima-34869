b8187f23e9e460b77b90060f13dbdd034775af38
javelin-ken <tf_javelin5296@yahoo.co.jp>
2021-03-30 17:32:31 +0900

    エラーメッセージの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,24 +5,28 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :condition
-  # belongs_to :shippig_cost
+  belongs_to :shipping_cost
   belongs_to :shipping_origin
   belongs_to :shipping_day
   has_one_attached :image
   belongs_to :user
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_cost_id, numericality: { other_than: 1 }
-  validates :shipping_origin_id, numericality: { other_than: 1 }
-  validates :shipping_days_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id,
+    validates :condition_id 
+    validates :shipping_cost_id 
+    validates :shipping_origin_id 
+    validates :shipping_days_id
+  end
+
   
 
   with_options presence: true do
     validates :image
     validates :name
-    validates :description
-    validates :price
+    validates :description 
+    validates :price, inclusion: { in: 300..9_999_999 , message: 'Out of setting range' }
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
   end
-
+  validates :price, numericality: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+  
+
+    it 'imageとname、description、category、condition、shipping_cost、shipping_origin、shipping_days、priceがあれば保存できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'imageが空では保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'nameが空では保存できないこと' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'descriptionが空では保存できないこと' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'categoryのidが1では保存できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+
+    it 'conditionのidが1では保存できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+
+    it 'shipping_costのidが1では保存できない' do
+      @item.shipping_cost_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
+    end
+
+    it 'shipping_originのidが1では保存できない' do
+      @item.shipping_origin_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping origin must be other than 1")
+    end
+
+    it 'shipping_daysのidが1では保存できない' do
+      @item.shipping_days_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping days must be other than 1")
+    end
+
+    it 'priceが空では保存できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが英字では登録できない' do
+      @item.price = 'one thousand'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number")
+    end
+
+
+
+    it 'priceが大文字の数字では登録できない' do
+      @item.price = '３００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number")
+    end
+
+    it 'priceは299円以下は保存できないこと' do
+      @item.price = '100'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it 'priceは10,000,000以上は保存できないこと' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it 'userが紐付いていないと保存できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
+  end
 end
